9166aeb8b3d012cd65dc7730bc3d4623b0256aec
dvalle <dvalleit@github.com>
2024-11-27 18:55:21 -0300

    fix

--- index.js
+++ index.js
@@ -34,15 +34,15 @@ async function run(){
 }
 
 await core.summary
-.addHeading('Test Results')
-.addCodeBlock(generateTestResults(), "js")
-.addTable([
-    [{data: 'File', header: true}, {data: 'Result', header: true}],
-    ['foo.js', 'Pass ✅'],
-    ['bar.js', 'Fail ❌'],
-    ['test.js', 'Pass ✅']
-])
-.addLink('View staging deployment!', 'https://github.com')
-.write()
+    .addHeading('Test Results')
+    .addCodeBlock(generateTestResults(), "js")
+    .addTable([
+        [{data: 'File', header: true}, {data: 'Result', header: true}],
+        ['foo.js', 'Pass ✅'],
+        ['bar.js', 'Fail ❌'],
+        ['test.js', 'Pass ✅']
+    ])
+    .addLink('View staging deployment!', 'https://github.com')
+    .write()
 
 run();
