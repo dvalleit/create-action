0df200e67e1bd59240a703f5464d0f420cfca5bf
dvalle <dvalleit@github.com>
2024-11-27 18:29:05 -0300

    fix

--- dist/index.js
+++ dist/index.js
@@ -31827,8 +31827,14 @@ async function run(){
         'X-GitHub-Api-Version': '2022-11-28'
         }
     });
+    const link = request.headers.link
+// <https://api.github.com/repositories/895224690/branches?per_page=1&page=2>; rel="next", <https://api.github.com/repositories/895224690/branches?per_page=1&page=3>; rel="last"
+    console.log(link)
 
-    console.log(request_branches.headers.link)
+    const re = /.*page=(.*)>; rel="last/i;
+    const found = str.match(re);
+
+    console.log(found); 
 
     // for await (const {data} of iterator) {
     //     console.log(data)
