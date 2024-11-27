1100c87b1be35881c6253013d13e4a44f30ec229
dvalle <dvalleit@github.com>
2024-11-27 17:59:54 -0300

    fix

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-con
+
 // const octokit = core.getInput('TOKEN')
 const octokit = new Octokit({ 
     auth: process.env.TOKEN,
   });
 
-
-const iterator = await octokit.paginate.iterator('GET /repos/{owner}/{repo}/branches', {
-    owner: 'OWNER',
-    repo: 'REPO',
-    per_page: 1,
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
-
-  
-// for await (const {data} of iterator) {
-//     for (const issue of data) {
+async function run(){
         
-//     }
-// }
-console.log(iterator)
+    const iterator = await octokit.paginate.iterator('GET /repos/{owner}/{repo}/branches', {
+        owner: 'OWNER',
+        repo: 'REPO',
+        per_page: 1,
+        headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+        }
+    });
+
+
+    // for await (const {data} of iterator) {
+    //     for (const issue of data) {
+            
+    //     }
+    // }
+    console.log(iterator)
+}
