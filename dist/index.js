079df5c00f9e5890aff70f0c4ffed698f5e474e8
dvalle <dvalleit@github.com>
2024-11-28 09:23:38 -0300

    fix

--- dist/index.js
+++ dist/index.js
@@ -31874,7 +31874,7 @@ async function run(){
             })
             // console.log(branchSpecs.data.commit.commit.author.date)
             const commitDate = branchSpecs.data.commit.commit.author.date
-            console.log(commitDate)
+            console.log(Math.abs(currentDate - commitDate))
             
 
         }
