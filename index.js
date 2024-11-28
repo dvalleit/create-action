7a6f72b1f5f9dcf13cde22da2356faff29e078e1
dvalle <dvalleit@github.com>
2024-11-28 10:28:59 -0300

    fix

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ async function run(){
         .addTable([
             [{data: 'Metric', header: true}, {data: 'Value', header: true}, {data: 'Status', header: true}],
             ['Amount of Branches', amountBranches, 'Pass ✅'],
-            ['bar.js', core.summary.addLink(hola,"github.com"), 'Fail ❌'],
+            ['bar.js', ' [link](https://github.com)', 'Fail ❌'],
             ['test.js', amountBranches, 'Pass ✅']
         ])
         .addLink('View staging deployment!', 'https://github.com')
