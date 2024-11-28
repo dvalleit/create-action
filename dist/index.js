e3bd569da11cae6514492609f9dce364cdb05fb9
dvalle <dvalleit@github.com>
2024-11-28 08:24:48 -0300

    fix

--- dist/index.js
+++ dist/index.js
@@ -31827,16 +31827,22 @@ async function run(){
         'X-GitHub-Api-Version': '2022-11-28'
         }
     });
-    const link = request_branches.headers.link
-// <https://api.github.com/repositories/895224690/branches?per_page=1&page=2>; rel="next", <https://api.github.com/repositories/895224690/branches?per_page=1&page=3>; rel="last"
-    console.log(link)
 
-    // const re = /.*page=(.*)>; rel="last/i;
+    const link = request_branches.headers.link
     const lastPattern = /(?<=<[\S]*&page=)([0-9]*)(?=>; rel="last")/i;
     const found = link.match(lastPattern);
-
     const amountBranches = found[0];
     console.log(amountBranches)
+
+    const listedBranches = await octokit.request('GET /repos/{owner}/{repo}/branches', {
+        owner: 'dvalleit',
+        repo: 'create-action',
+        per_page: 100,
+        headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    console.log(listedBranches)
     // for await (const {data} of iterator) {
     //     console.log(data)
     // }
